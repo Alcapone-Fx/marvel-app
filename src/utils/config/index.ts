4556a031e8abb2190d72981508b7a436a5ffcef7
Felix Navas <fnavas@applaudostudios.com>
2021-06-29 22:52:23 -0600

    ✨ App general configs

---
+++ src/utils/config/index.ts
@@ -0,0 +1,10 @@
+export const charactersOrder = {
+  nameAsc: 'name',
+  nameDesc: '-name',
+};
+
+export const filterOptions = {
+  name: 'nameStartsWith',
+  comics: 'comics',
+  stories: 'stories',
+};
