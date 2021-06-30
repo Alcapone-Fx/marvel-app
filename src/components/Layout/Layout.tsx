bd6cd2ac29c42d1d7640d5b73f65f3b1de022f8c
Felix Navas <fnavas@applaudostudios.com>
2021-06-29 22:57:38 -0600

    ✨ Update Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,40 @@
+import { useHistory, useLocation } from 'react-router-dom';
+
+import { paths } from 'utils/paths';
+
 const Layout = ({ children }) => {
+  const { pathname } = useLocation();
+  const { push } = useHistory();
+
+  const getNavItemsClassName = (path) =>
+    `flex items-center px-2 h-full text-lg tracking-wider cursor-pointer ${
+      pathname === path ? 'bg-blue-400' : ''
+    } hover:bg-gray-50 hover:text-gray-600`;
+
   return (
-    <div className="container h-screen">
+    <div className="h-screen">
       <header className="flex h-16 justify-end bg-gray-600">
         <nav>
           <ul className="flex h-full items-center text-gray-50">
-            <li className="mr-8 text-lg">Characters</li>
-            <li className="mr-8 text-lg">Comics</li>
-            <li className="mr-8 text-lg">Stories</li>
+            <li
+              className={getNavItemsClassName(paths.characters)}
+              onClick={() => push(paths.characters)}
+            >
+              Characters
+            </li>
+            <li className={getNavItemsClassName(paths.comics)}>
+              Comics
+            </li>
+            <li className={`mr-8 ${getNavItemsClassName(paths.stories)}`}>
+              Stories
+            </li>
           </ul>
         </nav>
       </header>
       <main>
-        <article className="container">{children}</article>
+        <article>
+          <div className="w-2/3 mx-auto p-4">{children}</div>
+        </article>
       </main>
     </div>
   );
