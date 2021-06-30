fefafe9a5aae64c59aa61bf68347565511e1449e
Felix Navas <fnavas@applaudostudios.com>
2021-06-29 23:00:32 -0600

    ✨ Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import Characters from 'pages/Characters';
+import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import Layout from 'components/Layout';
+import Characters from 'pages/Characters';
+import NotFound from 'pages/NotFound';
+import { paths } from 'utils/paths';
 
 const App = () => {
   return (
-    <Layout>
-      <Characters />
-    </Layout>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path={paths.characters} component={Characters} />
+          <Route exact path="/">
+            <Redirect to={paths.characters} />
+          </Route>
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
+    </Router>
   );
 };
 
